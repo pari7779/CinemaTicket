143c2e58856f7b6f7b97c34b509d82f9f2c47c0f
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-03-11 12:40:34 +0330

    env-deleted

--- cinemticket/movie/models.py
+++ cinemticket/movie/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-class Theater(models.Model):
-    pass
-
 
 class Movie(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    star=models.FloatField()
+    rate=models.IntegerField()
+    
+
 
 class Sans(models.Model):
     pass
+
+class Ticket(models.Model):
+    pass
+
