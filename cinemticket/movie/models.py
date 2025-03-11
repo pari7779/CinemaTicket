2647c5a27802dab54f783df1f14598b712e1404e
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-03-11 15:53:02 +0330

    Actor_model_added

--- cinemticket/movie/models.py
+++ cinemticket/movie/models.py
@@ -3,14 +3,25 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=100)
-    star=models.FloatField()
-    rate=models.IntegerField()
+    star = models.FloatField()
+    vote = models.IntegerField()
+    director_name = models.CharField(max_length=100)
+    cast = models.ManyToManyField(Actor,related_name='movies')
+    cinema = models.ForeignKey(to=Cinema)
     
-
-
+    
 class Sans(models.Model):
     pass
 
 class Ticket(models.Model):
     pass
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=100)
+    
+
+class Actor(models.Model): #related to Actors page#every related page shows description and 
+    name = models.CharField(max_length=100)
+    biography=models.TextField(max_length=1000)
+    movie=models.ManyToManyField(Movie)    
 
